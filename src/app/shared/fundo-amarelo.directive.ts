f2d1b7eb2dafc3ea4c8a247e1c42ed9f2a635e4c
emanoelcampos <emanoel.cv@gmail.com>
2022-10-13 16:19:11 -0300

    input e property binding

--- src/app/shared/fundo-amarelo.directive.ts
+++ src/app/shared/fundo-amarelo.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: 'p[appFundoAmarelo]'
+  selector: '[appFundoAmarelo]'
 })
 export class FundoAmareloDirective {
 
